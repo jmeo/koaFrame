0ea1e834780fc6763d43bf90a32c031868bd65bd
jmeo <jmeo.mao@timanetworks.com>
2017-07-14 14:07:53 +0800

    scan add all router interfaces

--- src/common/routerScan.js
+++ src/common/routerScan.js
@@ -1,8 +1,11 @@
 /**
  * Created by jmeo on 2016/11/7.
  */
+'use strict';
 var findFile = require("./findFile"),
     path = require("path");
+const router = require('koa-router')();
+
 
 /**
  * 路由配置
@@ -10,9 +13,27 @@ var findFile = require("./findFile"),
  */
 function routerScan(app){
     var files = findFile(path.join(__dirname,'../router'),'router.js');
+    var stack = [];
     for(var k in files){
-        app.use(require(files[k]).routes());
+        let route = require(files[k]).routes();
+        stack.push(route.router.stack)
+        app.use(route);
     }
+    // app.use('/links',function (ctx) {
+    //     ctx.body = stack;
+    // });
+
+    router.get('/appServices',(ctx,next)=>{
+        var tm_stack = stack[0];
+        console.log(tm_stack);
+        let list = [];
+        for(var i=0;i<tm_stack.length;i++){
+            let item_obj = { 'method':tm_stack[i]['methods'][0] == 'HEAD' ? tm_stack[i]['methods'][1] : tm_stack[i]['methods'][0],'path':tm_stack[i]['path']};
+            list.push(item_obj);
+        }
+        ctx.body = list;
+    });
+    app.use(router.routes());
 }
 
 module.exports = routerScan;
